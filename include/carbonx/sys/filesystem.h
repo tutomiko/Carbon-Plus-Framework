bcddf3fce4781965ac5f0a7b2ea816f34fa9bc12
tutomiko <98809576+tutomiko@users.noreply.github.com>
2022-11-10 18:05:52 +0200

    Add files via upload

---
+++ include/carbonx/sys/filesystem.h
@@ -0,0 +1,37 @@
+#
+#pragma once
+#
+#pragma region <imports>
+#pragma region "platform-dependent imports"
+
+#pragma endregion
+#
+#pragma region "platform-independent imports"
+
+#pragma endregion
+#
+#pragma region "3rd-party imports"
+
+#pragma endregion
+#
+#pragma region "shared imports"
+#include <carbonx/cbevobase.h>
+#include <carbonx/io/iobase.h>
+#include <carbonx/sys/paths.h>
+#include <carbonx/sys/file.h>
+#include <carbonx/sys/directories.h>
+#pragma endregion
+#
+#pragma region "local imports"
+
+#pragma endregion
+#pragma endregion
+#
+#
+namespace carbon {
+	namespace sys {
+
+	}
+}
+
+
