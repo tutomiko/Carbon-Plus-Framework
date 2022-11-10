4f7f2d518f1d36800eb729f9bd144f7153c328d8
tutomiko <98809576+tutomiko@users.noreply.github.com>
2022-11-10 18:11:47 +0200

    Add files via upload

---
+++ test/Test.cpp
@@ -0,0 +1,31 @@
+#
+#pragma region <imports>
+#pragma region "export header imports"
+
+#pragma endregion
+#
+#pragma region "platform-dependent imports"
+#include <Windows.h>
+#pragma endregion
+#
+#pragma region "platform-independent imports"
+#include <iostream>
+#pragma endregion
+#
+#pragma region "3rd-party imports"
+#include "../include/carbon"
+#pragma endregion
+#
+#pragma region "shared imports"
+
+#pragma endregion
+#
+#pragma region "local imports"
+
+#pragma endregion         
+#pragma endregion
+#
+#
+int _cdecl main(int argc, char** argv) {
+	
+}
