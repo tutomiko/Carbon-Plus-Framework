49eb3d61a750db5d9fc7a0c4e637e75b34805a95
tutomiko <98809576+tutomiko@users.noreply.github.com>
2022-11-14 03:15:49 +0200

    Add files via upload

---
+++ framework/config.h
@@ -0,0 +1,18 @@
+#
+#pragma once
+#
+#
+#pragma region <project build info>
+
+#pragma endregion
+
+
+#pragma region <functionality toggle configurations>
+
+#pragma endregion
+
+
+#pragma region <configurations>
+
+#pragma endregion
+
