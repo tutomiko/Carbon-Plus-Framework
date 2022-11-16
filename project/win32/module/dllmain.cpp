3b3ef228f3f2d732a6471c7f3a1b49beb3045174
tutomiko <98809576+tutomiko@users.noreply.github.com>
2022-11-16 08:33:54 +0200

    Add files via upload

---
+++ project/win32/module/dllmain.cpp
@@ -0,0 +1,62 @@
+/*
+ * Written by Tuomas Kontiainen <https://www.github.com/tutomiko>
+ *
+ * Copyright (c) 2022, Tuomas Kontiainen
+ *
+ * This file is a part of Carbon Plus Framework <https://github.com/tutomiko/Carbon-Plus-Framework>
+ * Carbon Plus Framework is a free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later versions.
+ *
+ * Carbon Plus Framework is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Carbon Plus Framework. If not, see <http://www.gnu.org/licenses/>.
+ */
+#
+#
+#pragma region <imports>
+#pragma region "export header imports"
+
+#pragma endregion
+#
+#pragma region "platform-dependent imports"
+#include <Windows.h>
+#pragma endregion
+#
+#pragma region "platform-independent imports"
+
+#pragma endregion
+#
+#pragma region "3rd-party imports"
+
+#pragma endregion
+#
+#pragma region "shared imports"
+
+#pragma endregion
+#
+#pragma region "local imports"
+
+#pragma endregion
+#pragma endregion
+#
+#
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
+    switch (ul_reason_for_call) {
+    case DLL_PROCESS_ATTACH:
+        break;
+    case DLL_PROCESS_DETACH:
+        break;
+    case DLL_THREAD_ATTACH:
+        break;
+    case DLL_THREAD_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
